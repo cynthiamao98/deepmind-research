ea181c7ee2bd44594169af1e15d5fc72a506f0a8
Hung Vuong <vuongtienhung2501@gmail.com>
2020-03-31 23:04:31 -0400

    add hmm profile generation and potts model params (pseudolikelihood/pseudo_bias) from ccmpred

--- alphafold_casp13/feature_generation.py
+++ alphafold_casp13/feature_generation.py
@@ -4,10 +4,11 @@ import tensorflow.compat.v1 as tf
 import os
 
 import cProfile
-from contacts_dataset import *
 import parser
 
 import numpy as np
+from contacts_dataset import *
+from ccmpred.raw import *
 
 tf.enable_eager_execution()
 
@@ -46,7 +47,8 @@ features = [
       "profile",
       "hhblits_profile",
       "residue_index",
-      "between_segment_residues"
+      "between_segment_residues",
+      "mutual_information"
     ]
 tf_dataset = create_tf_dataset('./casp13_data/T1006/T1006.tfrec', features)
 # sequence = 'MGSSHHHHHHSSGLVPRGSHMEGKKILVTGGTGQVARPVAEALAERNEVWCLGRFGTPGVEKELNDRGITTFHWDMDDPGAAAYEGLPDDFTHVLHSAVRRGEDGDVNAAVEVNSVACGRLMTHCRGAEAFLFVSTGALYKRQTLDHAYTEDDPVDGVADWLPAYPVGKIAAEGAVRAFAQVLNLPTTIARLNIAYGPGGYGGVPMLYFKRMLAGEPIPVPKEGQNWCSLLHTDDLVAHVPRLWEAAATPATLVNWGGDEAVGITDCVRYLEELTGVRARLVPSEVTRETYRFDPTRRREITGPCRVPWREGVRRTLQALHPEHLPSESRHSAV'
@@ -62,9 +64,7 @@ with open('./T1006.txt', 'a') as f:
         print('\n', file=f)
 # f.close()
 
-
-import numpy as np
-
+# 1. aatype
 def sequence_to_onehot(sequence):
   """Maps the given sequence into a one-hot encoded matrix."""
   mapping = {aa: i for i, aa in enumerate('ARNDCQEGHILKMFPSTWYVX')}
@@ -159,3 +159,51 @@ def non_gapped_profile(amino_acids):
     # recon_data = gap_matrix(hhblits_a3m_sequences)
     # print('recon_data', recon_data)
     # print('difference', np.sum(gap_m.numpy()-recon_data))
+
+# hmm profile
+def extract_hmm_profile(hhm_file, sequence, asterisks_replace=0.0):
+  """Extracts information from the hmm file and replaces asterisks."""
+  profile_part = hhm_file.split('#')[-1]
+  profile_part = profile_part.split('\n')
+  whole_profile = [i.split() for i in profile_part]
+  # This part strips away the header and the footer.
+  whole_profile = whole_profile[5:-2]
+  gap_profile = np.zeros((len(sequence), 10))
+  aa_profile = np.zeros((len(sequence), 20))
+  count_aa = 0
+  count_gap = 0
+  for line_values in whole_profile:
+    if len(line_values) == 23:
+      # The first and the last values in line_values are metadata, skip them.
+      for j, t in enumerate(line_values[2:-1]):
+        aa_profile[count_aa, j] = (
+            2**(-float(t) / 1000.) if t != '*' else asterisks_replace)
+      count_aa += 1
+    elif len(line_values) == 10:
+      for j, t in enumerate(line_values):
+        gap_profile[count_gap, j] = (
+            2**(-float(t) / 1000.) if t != '*' else asterisks_replace)
+      count_gap += 1
+    elif not line_values:
+      pass
+    else:
+      raise ValueError('Wrong length of line %s hhm file. Expected 0, 10 or 23'
+                       'got %d'%(line_values, len(line_values)))
+  hmm_profile = np.hstack([aa_profile, gap_profile])
+  assert len(hmm_profile) == len(sequence)
+  return tf.convert_to_tensor(hmm_profile)
+
+# potts model params
+def pseudo_params(filepath):
+      raw_output = parse_msgpack(filepath)
+      x_pair = raw_output.x_pair #pseudolikelihood
+      x_single = raw_output.x_single #pseudobias
+      # adding entries for X and set those values to 0:
+      #bias: add a coln at index 20 for the residue X
+      pseudo_bias = np.insert(x_single, 20, np.zeros(x_single.shape[0]), axis=1)
+      # pseudolikelihood
+      h, w, c, d = x_pair.shape
+      pseudolikelihood = np.insert(x_pair, 20, np.zeros((h, w, c)), axis=2)
+      pseudolikelihood = np.insert(pseudolikelihood, 20, np.zeros((h, w, d+1)), axis=3)
+      pseudolikelihood = pseudolikelihood.reshape((h, w, 484))
+      return tf.convert_to_tensor(pseudo_bias), tf_convert_to_tensor(pseudolikelihood)
--- alphafold_casp13/parser.py
+++ alphafold_casp13/parser.py
@@ -14,3 +14,16 @@ def parse_msa_with_a3m(filepath):
            cnt += 1
     return msa
 
+def strip_lowercase_msa(filepath):
+   """
+  stripping the lowercase in a3m format (the deleted residues) so that all msa have the same length
+  """
+  a3m_msa = parse_msa_with_a3m(filepath)
+  msa_seqs = []
+  for seq in a3m_seqs:
+    none_lowercase_letters = []
+    for letter in seq:
+      if letter.islower() == False:
+        none_lowercase_list.append(letter)
+    msa_seqs.append(''.join(none_lowercase_letters))
+  return msa_seqs  
